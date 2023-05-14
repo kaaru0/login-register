9010c80dd245d8e5c455776e33e12f97cc908390
kaaru0 <ongkaaru@gmail.com>
2023-05-14 21:46:49 +0800

    initialized mysql database

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,17 @@ const db = mysql.createConnection({
     password: "",
     database: "signup"
 })
+
+app.post('/signup', (req,res) => {
+    const sql = "INSERT INTO login ('name','email','password') VALUES (?)"
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+})
+
+app.listen(8081, ()=> {
+    console.log("successful connection to database")
+})
+
